b917f6382ce8c57de2a19f26dacb90e42b85ecdb
igopod <6089419+igopod@users.noreply.github.com>
2021-08-12 17:35:31 +0300

    added contacts page

--- src/main.go
+++ src/main.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func mainPageFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to appointment app called mementoludi!</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", mainPageFunc)
 	fmt.Println("Starting server on localhost:3000...")
 	fmt.Println("Press Ctrl + C to stop the server")
 	http.ListenAndServe(":3000", nil)
