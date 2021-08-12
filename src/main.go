3a07428bdae7ec6e955440d25ee69ad008adcb84
igopod <6089419+igopod@users.noreply.github.com>
2021-08-12 17:40:12 +0300

    added contacts page and rename main page function

--- src/main.go
+++ src/main.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 )
 
-func mainPageFunc(w http.ResponseWriter, r *http.Request) {
+func mainPageHandle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to appointment app called mementoludi!</h1>")
 }
 
+func contactsHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contacts page</h1>")
+}
+
 func main() {
-	http.HandleFunc("/", mainPageFunc)
+	http.HandleFunc("/", mainPageHandle)
+	http.HandleFunc("/contacts", contactsHandle)
 	fmt.Println("Starting server on localhost:3000...")
 	fmt.Println("Press Ctrl + C to stop the server")
 	http.ListenAndServe(":3000", nil)
